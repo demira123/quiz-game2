20c8fe693423079429044457ac37c08e2b0af693
demira123 <75173062+demira123@users.noreply.github.com>
2021-04-03 16:06:10 -0400

    Add files via upload

---
+++ Question.js
@@ -0,0 +1,59 @@
+class Question {
+    
+      constructor() {
+        this.title =createElement('h1')
+        this.input1 = createInput("Name");
+        this.input2=createInput("Enter Correct option")
+        this.button = createButton('Submit');
+        this.question = createElement('h3')
+        this.option1 =createElement('h4')
+        this.option2= createElement('h4')
+        this.option3 = createElement('h4')
+        this.option4 =createElement('h4')
+      }
+      hide(){
+       this.input2.hide();
+        this.button.hide()
+        this.title.hide()
+        this.input1.hide();
+      }
+    
+      display(){
+        this.title.html("My Quiz Game");
+        this.title.position(350, 0); //400 
+    
+        this.question.html("Question: What month of the year has 28 days?");
+        this.question.position(150,80);
+    
+        this.option1.html("1: January")
+        this.option1.position(150,100)
+        this.option2.html("2: September")
+        this.option2.position(150,120)
+        this.option3.html("3: February")
+        this.option3.position(150,140)
+        this.option4.html("4: All of them")
+        this.option4.position(150,160)
+
+        
+        this.button.position(100,233)
+        this.input1.position(150,230)
+        this.input2.position(250,233)
+
+        this.button.mousePressed(()=>{
+          this.title.hide();
+          this.input1.hide();
+          this.input2.hide();
+          this.button.hide();
+          contestant.name = this.input1.value();
+          contestant.answer = this.input2.value();
+          contestantCount+=1;
+          contestant.index = contestantCount;
+          contestant.update();
+          contestant.updateCount(contestantCount);
+        })
+      }
+
+}
+    
+      
+    
